44073143a4abfcf4eb531f6027677b7fed647a6c
8kengo <mi191324@edu.okinawa-ct.ac.jp>
2020-09-15 01:24:58 +0900

    SQLの操作変更

--- familyquestion.py
+++ familyquestion.py
@@ -6,32 +6,33 @@ from click.types import File
 from flask import Request, Response
 from flask.helpers import make_response
 from typing import Union
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 connection_name = os.environ['INSTANCE_CONNECTION_NAME']
 db_password = os.environ['DATABASE_USER_PASSWORD']
 db_name = os.environ['NAME']
 db_user = os.environ['USER']
 driver_name = 'mysql+pymysql'
-query_string = dict({"unix_sock": "/cloudsql/{}".format(connection_name)})
+query_string = dict({"unix_socket": "/cloudsql/{}".format(connection_name)})
+Base=declarative_base()
 
 def familyquestion(request: Request) -> Union[Response, None]:
+    engine = create_engine('farmily-meal:asia-northeast1:questions')
+    SessionClass=sessionmaker(engine) 
+    session=SessionClass()
     request_json = request.get_json()
     textdata = request_json['file']
-    stmt = sqlalchemy.text('INSERT INTO entries (guestName, content) value({0}, {1});').format("Question1", textdata)
-    db = sqlalchemy.create_engine(
-        sqlalchemy.engine.url.URL(
-            drivername=driver_name,
-            username=db_user,
-            password=db_password,
-            database=db_name,
-            query=query_string,),
-            pool_size=5,
-            max_overflow=2,
-            pool_timeout=30,
-            pool_recycle=1800)
-    try:
-        with db.connect() as conn:
-            conn.execute(stmt)
-    except Exception as e:
-        return 'error: {}'.format(str(e))
-    return ('error')
+    questiona=User(guestName="Question1", content=textdata)
+    session.add(questiona)
+    session.commit()
+    return ('finish')
+
+    class User(Base):
+        _tablename_="question"
+        question_id=Column(Integer, primary_key=True)
+        guestName=Column(String(255))
+        content=Column(String(255))
