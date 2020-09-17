9538887391ee8275e50fb36a5339d75154cbdc02
8kengo <mi191324@edu.okinawa-ct.ac.jp>
2020-09-17 10:40:06 +0900

    Calender_phot.py作成

---
+++ Calender_phot.py
@@ -0,0 +1,31 @@
+import os
+
+from google.cloud import storage
+from click.types import File
+from flask import Request, Response
+from flask.helpers import make_response
+from typing import Union
+
+meal_phot = os.environ['meal_phot']
+
+def hello_world(request: Request) -> Union[Response, None]:
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    request_json = request.get_json()
+    daydate = request_json['date']
+    filename = daydate + ".jpeg"
+
+    gcs = storage.Client()
+    bucket = gcs.get_bucket(meal_phot)
+
+    blob = bucket.get_blob(filename)
+    if (blob == None):
+        blob = bucket.get_blob("Noimage.jpg")
+
+    return make_response(filename)
